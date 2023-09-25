29fbfc9a8bc1d885e54e110f26b9fddc95086e00
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-25 19:24:01 -0400

    connectingfirestore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Auth } from './components/auth';
+import {db} from './config/firebase'
+import {getDocs} from 'firebase/firestore'
 
 function App() {
+  const [movieList, setMovieList] = useState([])
+
+  useEffect(()=>{
+    const getMovieList = async() =>{
+      const data = await getDocs()
+       
+    }
+  })
+  
   return (
     <div className="flex items-center h-screen justify-center">
       <Auth/>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider, FacebookAuthProvider} from 'firebase/auth'
+import {getFirestore} from 'firebase/firestore'
 
 
 const firebaseConfig = {
@@ -17,3 +18,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider()
 export const fbProvider = new FacebookAuthProvider()
+
+export const db = getFirestore(app)
