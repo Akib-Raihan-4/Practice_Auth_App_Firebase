bdc3894d8b6fc2d785982cc7acc78f5291970b05
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-25 17:45:11 -0400

    can sign in using google account

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,23 +1,44 @@
 import React, { useState } from 'react'
-import {auth} from '../config/firebase'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {auth, googleProvider, fbProvider} from '../config/firebase'
+import {createUserWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
 
 export const Auth = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
     // console.log(auth?.currentUser?.email)
+    // console.log(auth?.currentUser?.photoURL)
 
     const signIn = async() =>{
         try{
             await createUserWithEmailAndPassword(auth, email, password)
         }catch(err){
             console.log(err)
-        }
-        
-        
+        }        
     }
 
+    const signInGoogle = async() =>{
+        try{
+            await signInWithPopup(auth, googleProvider)
+        }catch(err){
+            console.log(err)
+        }
+    }
+    // const signInFB = async() =>{
+    //     try{
+    //         await signInWithPopup(auth, fbProvider)
+    //     }catch(err){
+    //         console.log(err)
+    //     }
+    // }
+
+    const logout = async() => {
+        try{
+            await signOut(auth)
+        }catch(err){
+            console.log(err)
+        }
+    }
 
   return (
     <div>
@@ -28,7 +49,10 @@ export const Auth = () => {
         type='password'
         onChange={(e) => setPassword(e.target.value)}
         />
-        <button className='bg-[#3f4cd7] text-white px-5 rounded-lg py-2 ml-2' onClick={signIn}>Sign In</button>
+        <button className='bg-[#3f4cd7] text-white px-5 rounded-lg py-2 mx-2' onClick={signIn}>Sign In</button>
+        <button className='bg-[#3f4cd7] text-white px-5 rounded-lg py-2 mx-2' onClick={signInGoogle}>Google In</button>
+        <button className='bg-[#3f4cd7] text-white px-5 rounded-lg py-2 mx-2' onClick={logout}>Log Out</button>
+        {/* <button className='bg-[#3f4cd7] text-white px-5 rounded-lg py-2 mx-2' onClick={signInFB}>Facbook In</button> */}
     </div>
   )
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import {getAuth, GoogleAuthProvider, FacebookAuthProvider} from 'firebase/auth'
 
 
 const firebaseConfig = {
@@ -15,3 +15,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
+export const fbProvider = new FacebookAuthProvider()
